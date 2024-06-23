bdb4727730e50d611e3618bae6da23a19625e3a6
Stamper221 <110437317+Stamper221@users.noreply.github.com>
2024-06-23 18:39:08 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -44,7 +44,7 @@ def custom_css():
         }
         .section {
             padding: 40px 0;
-            margin-top: 300px;
+            margin-top: 220px;
         }
         .nav-trigger {
             position: fixed;
